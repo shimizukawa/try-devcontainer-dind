901a93336e7157ebff0c251f2cc6adc26a035546
shimizukawa <shimizukawa@gmail.com>
2021-08-25 21:31:15 +0900

    pull docker images on create devcontainer

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,6 +1,10 @@
-cp .env.sample .env \
-&& docker-compose pull \
-&& docker-compose build \
-&& docker-compose up -d db \
-&& docker-compose run frontend npm install \
-&& docker-compose run backend python apps/manage.py migrate
+cp .env.sample .env
+docker pull ghcr.io/beproud/try-devcontainer-dind/backend:latest
+docker pull ghcr.io/beproud/try-devcontainer-dind/frontend:latest
+docker pull ghcr.io/beproud/try-devcontainer-dind/nginx:latest
+docker pull postgres:11
+docker-compose pull
+docker-compose build
+docker-compose up -d db
+docker-compose run frontend npm install
+docker-compose run backend python apps/manage.py migrate
