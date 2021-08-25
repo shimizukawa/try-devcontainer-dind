e33878eb42c83bfedcd64d162f6c3e2ef11eb4d4
shimizukawa <shimizukawa@gmail.com>
2021-08-26 00:03:40 +0900

    use docker cache with buildkit on compose

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,10 +1,5 @@
 cp .env.sample .env
-# docker pull ghcr.io/beproud/try-devcontainer-dind/backend:latest
-# docker pull ghcr.io/beproud/try-devcontainer-dind/frontend:latest
-# docker pull ghcr.io/beproud/try-devcontainer-dind/nginx:latest
-# docker pull postgres:11
-docker-compose pull
-docker-compose build
+docker-compose -f docker-compose.yml -f docker-compose.build.yml build --parallel
 docker-compose up -d db
 docker-compose run frontend npm install
 docker-compose run backend python apps/manage.py migrate
