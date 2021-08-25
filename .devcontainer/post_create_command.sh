48edcd193f29e76d4b4c7c2f1632d20de97128a5
shimizukawa <shimizukawa@gmail.com>
2021-08-25 23:20:17 +0900

    set buildkit env to use docker cache

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,8 +1,8 @@
 cp .env.sample .env
-docker pull ghcr.io/beproud/try-devcontainer-dind/backend:latest
-docker pull ghcr.io/beproud/try-devcontainer-dind/frontend:latest
-docker pull ghcr.io/beproud/try-devcontainer-dind/nginx:latest
-docker pull postgres:11
+# docker pull ghcr.io/beproud/try-devcontainer-dind/backend:latest
+# docker pull ghcr.io/beproud/try-devcontainer-dind/frontend:latest
+# docker pull ghcr.io/beproud/try-devcontainer-dind/nginx:latest
+# docker pull postgres:11
 docker-compose pull
 docker-compose build
 docker-compose up -d db
